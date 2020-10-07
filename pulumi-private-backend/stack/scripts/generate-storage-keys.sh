5f48dedcbcde158cbe9cbbc6675426edd88a7e4b
Jim Counts <jamesrcounts@outlook.com>
2020-10-07 14:14:08 +0000

    Complete Token Script

--- pulumi-private-backend/stack/scripts/generate-storage-keys.sh
+++ pulumi-private-backend/stack/scripts/generate-storage-keys.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-# To ensure the az CLI is logged in and out properly, run this task inside an Azure CLI DevOps task.
+# To ensure the az CLI is logged in and out properly, run this task inside an 
+# Azure CLI DevOps task.
 set -euo pipefail
+
+# Caluclate the token expiration time
+TOKEN_EXPIRATION=$(date -u -d "1 hour" '+%Y-%m-%dT%H:%MZ')
 
 # Generate a read-write SAS token for the private backend storage container
 TOKEN=$(
     az storage container generate-sas \
+        --account-name ${AZURE_STORAGE_ACCOUNT} \
         --name ${AZURE_STORAGE_CONTAINER} \
         --expiry ${TOKEN_EXPIRATION} \
         --permissions rw \
-        --account-name ${AZURE_STORAGE_ACCOUNT} \
         --https-only \
         --output tsv
 )
