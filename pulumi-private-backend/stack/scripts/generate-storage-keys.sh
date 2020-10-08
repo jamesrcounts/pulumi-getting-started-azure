e73d0ffeee9a4e7068ee25ae58872269619517e6
Jim Counts <jamesrcounts@outlook.com>
2020-10-08 00:59:25 +0000

    print commands

--- pulumi-private-backend/stack/scripts/generate-storage-keys.sh
+++ pulumi-private-backend/stack/scripts/generate-storage-keys.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # To ensure the az CLI is logged in and out properly, run this task inside an 
 # Azure CLI DevOps task.
-set -euo pipefail
+set -euox pipefail
 
 # Caluclate the token expiration time
 TOKEN_EXPIRATION=$(date -u -d "1 hour" '+%Y-%m-%dT%H:%MZ')
